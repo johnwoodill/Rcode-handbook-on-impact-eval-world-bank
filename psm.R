ee3f5527fe9aae3bcabd959eb49b7976cf715ac2
johnwoodill <johnwoodill@gmail.com>
2015-03-02 17:31:04 -1000

    Final PSM commit

--- psm.R
+++ psm.R
@@ -10,9 +10,11 @@
 
 library(dplyr)
 library(foreign)    # For converting *.dta
-library(nonrandom)  # stratifi-cation, matching and covariate adjustment by PSM
 library(Matching)
 library(MatchIt)
+library(ggplot2)
+library(survey)
+library(nonrandom)
 
 setwd("/home/john/Dropbox/UHM/Classes/Econ 610 - Economic Development/Problem Sets")
 
@@ -39,14 +41,89 @@ hh_98.df <- mutate(hh_98.df, lexptot = log(1 + exptot)) %>%
 # Impacts of program participation
 ###################################
 
-pscore(formula = dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil + egg,
-       data = hh_98.df)
+# First Regression (Unbalanced)
 
-a <- matchit(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil + egg,
-        data = hh_98.df)
+des1 <- svydesign(id = ~X,  weights = ~weight, data = hh_98.df)
+prog.lm <- svyglm(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil + egg, 
+                  design=des1, family = quasibinomial(link = "probit"))   
+X <- prog.lm$fitted
+Tr <- hh_98.df$dmmfd
+Y <- hh_98.df$lexptot
 
-MatchBalance(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil + egg,
-             data = hh_98.df)
+m.out <- Match(Tr = Tr, X = X, Y = Y, caliper = 0.001)
+summary(m.out)
 
-match(formula = dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil + egg,
-      data = hh_98.df)
+MatchBalance(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil + egg, data = hh_98.df, nboots = 500, ks = TRUE)
+
+ggplot() + 
+  geom_density(aes(x=m.out$index.control, linetype = '2')) +
+  geom_density(aes(x=m.out$index.treated, linetype = '3')) +
+  xlim(-300,1500) +
+  xlab("") +
+  scale_linetype_discrete(name = "", labels = c("Control", "Treated")) +
+  ggtitle("Control and Treated Densities")
+
+# Second Regression (Balanced)
+
+des1 <- svydesign(id = ~X,  weights = ~weight, data = hh_98.df)
+prog.lm <- svyglm(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil, 
+                  design=des1, family = quasibinomial(link = "probit"))   
+
+X <- prog.lm$fitted
+Tr <- hh_98.df$dmmfd
+Y <- hh_98.df$lexptot
+
+m.out <- Match(Tr = Tr, X = X, Y = Y, caliper = 0.001, M = 1, CommonSupport = TRUE)
+summary(m.out)
+
+ggplot() + 
+  geom_density(aes(x=m.out$index.control, linetype = '2')) +
+  geom_density(aes(x=m.out$index.treated, linetype = '3')) +
+  xlim(-300,1500) +
+  xlab("") +
+  scale_linetype_discrete(name = "", labels = c("Control", "Treated")) +
+  ggtitle("Control and Treated Densities")
+
+####################
+# Male Matching
+####################
+
+# Nearest Neighbor
+des1 <- svydesign(id = ~X,  weights = ~weight, data = hh_98.df)
+prog.lm <- svyglm(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil, 
+                  design=des1, family = quasibinomial(link = "probit"))   
+
+X <- prog.lm$fitted.values
+Tr <- hh_98.df$dmmfd
+Y <- hh_98.df$lexptot
+m.out <- Match(Tr = Tr, X = X, Y = Y, M = 1, caliper = 0.001, replace = TRUE)
+summary(m.out)
+
+# Stratification Matching
+m.out <- matchit(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil,
+                 data = hh_98.df, method = "nearest", distance = "probit", caliper = 0.001)
+summary(m.out)
+
+####################
+# Female Matching
+####################
+
+# Nearest Neighbor
+glm.female <- glm(dfmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil, family = binomial, data = hh_98.df)
+X <- glm.female$fitted
+Tr <- hh_98.df$dmmfd
+Y <- hh_98.df$lexptot
+m.out <- Match(Tr = Tr, X = X, Y = Y, caliper = 0.001, M = 1, replace = TRUE)
+summary(m.out)
+
+
+
+psm_m.lm <- pscore(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil, 
+                   name.pscore = "pscore", data = hh_98.df, family = quasibinomial(link = "probit"))
+summary(psm_m.lm)
+
+mathc <- matchit(dmmfd ~ sexhead + agehead + educhead + lnland + vaccess + pcirr + rice + wheat + milk + oil + egg, method = "nearest", data = hh_98.df)
+
+psm_m.match <- ps.match(object = psm_m.lm,
+                        matched.by = "pscore", )
+summary(psm_m.match)
